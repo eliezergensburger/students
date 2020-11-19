fa68966d956a0c3213d34d5ef80ce12ce6937beb
Eliezer Gensburger <eliezer.jct@gmail.com>
2020-11-19 22:14:28 +0200

    voila

---
+++ tests/entities/StudentTest.java
@@ -0,0 +1,28 @@
+package entities;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
